9c47885db16a60227f88d77abff2dfdbf93fae93
Henrik Nyh <henrik@nyh.se>
2015-09-05 09:46:10 +0200

    Add sanity tests for date comparison

--- test/fixme_test.exs
+++ test/fixme_test.exs
@@ -24,4 +24,11 @@ defmodule FIXMETest do
   test "does not raise before date" do
     FIXME._fixme {2015, 1, 1}, {2015, 1, 2}, "boom"
   end
+
+  test "sanity" do
+    # Let's be extra confident that we can compare dates as tuples.
+    assert {2015, 1, 1} > {2014, 12, 31}
+    assert {2015, 2, 1} > {2015, 1, 1}
+    assert {2015, 1, 2} > {2015, 1, 1}
+  end
 end
