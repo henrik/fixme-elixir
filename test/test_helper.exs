585b0e08688d6e557107f6d6ef11ef95f1ea5b28
Andrea Leopardi <an.leopardi@gmail.com>
2015-11-08 12:06:50 +0100

    Simplify the `assert_compile_time_raise` macro

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,13 +2,11 @@ ExUnit.start()
 
 defmodule CompileTimeAssertions do
   defmacro assert_compile_time_raise(expected_exception, expected_message, fun) do
-    # At compile-time, the fun is in AST form and thus cannot raise.
-    # At run-time, we will evaluate this AST, and it may raise.
-    fun_quoted_at_runtime = {:quote, [], [[do: fun]]}
+    fun = Macro.escape(fun)
 
     quote do
       assert_raise unquote(expected_exception), unquote(expected_message), fn ->
-        Code.eval_quoted(unquote(fun_quoted_at_runtime))
+        Code.eval_quoted(unquote(fun))
       end
     end
   end
