c7ae4ddd2deec8ef34120e69cdd54a0fe9411115
Henrik Nyh <henrik@nyh.se>
2020-06-19 17:20:03 +0100

    Cleanup

--- lib/fixme.ex
+++ lib/fixme.ex
@@ -6,27 +6,23 @@ defmodule FIXME do
   ) do
     {opts, _} = Code.eval_quoted(opts)
 
-    # Injectable "today" for tests.
+    # Injectable `today` for tests.
     current_date = opts[:today] || (:calendar.local_time |> elem(0))
 
-    # Opt into compile time warnings via the application get env
-    # Pass the additional option 'warn' to override the application setting
-    warn = case opts[:warn] do
-      nil ->
-        Application.get_env(:fixme, :warn, false)
-      warn ->
-        warn
-    end
+    warn? =
+      case opts[:warn] do
+        nil  -> Application.get_env(:fixme, :warn, false)
+        warn? -> warn?
+      end
 
     fixme_date = {year, month, day}
+    due? = current_date >= fixme_date
 
-    message = "Fix by #{year}-#{zeropad month}-#{zeropad day}: #{message}"
+    message = "Fix by #{year}-#{zeropad(month)}-#{zeropad(day)}: #{message}"
     cond do
-      current_date >= fixme_date ->
-        raise message
-      warn ->
-        IO.warn(message)
-      true -> true
+      due?  -> raise message
+      warn? -> IO.warn(message)
+      true  -> true
     end
   end
 
